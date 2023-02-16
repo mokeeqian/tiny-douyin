4322c8e4505495fb7539488ed1f441f3d61cff07
mokeeqian <mokeeqian@gmail.com>
2023-02-16 20:32:04 +0800

    cos配置常量化，解耦抽离

---
+++ src/dao/cos.go
@@ -0,0 +1,11 @@
+package dao
+
+const (
+	// 腾讯云对象存储配置
+	COS_BUCKET_NAME = "qian-1258498110"
+	COS_APP_ID      = "1258498110"
+	COS_REGION      = "nanjing"
+	COS_SECRET_ID   = "AKIDAJXkjSYR6ctvzrWyDDyVosiNo24xeDYe"
+	COS_SECRET_KEY  = "wc2C8qLcgldFcq6E3d2JLAk49yZS6Wt7"
+	COS_URL         = "https://qian-1258498110.cos.ap-nanjing.myqcloud.com"
+)
--- src/dao/mysql.go
+++ src/dao/mysql.go
@@ -15,13 +15,6 @@ import (
 
 const (
 	DRIVER = "mysql"
-
-	// 腾讯云对象存储配置
-	COS_BUCKET_NAME = "qian-1258498110"
-	COS_APP_ID      = "1258498110"
-	COS_REGION      = "nanjing"
-	COS_SECRET_ID   = "AKIDAJXkjSYR6ctvzrWyDDyVosiNo24xeDYe"
-	COS_SECRET_KEY  = "wc2C8qLcgldFcq6E3d2JLAk49yZS6Wt7"
 )
 
 var SqlSession *gorm.DB
